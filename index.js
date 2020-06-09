e8a12adf1fc397e56f706840aac7f08464f0a466
Max Yeo <mzwyeo@gmail.com>
2020-06-09 00:39:11 -0700

    sustain poc for now

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 $("#flip").click(function(){
 	$("#front, #back").addClass("toreverse");
 	window.setTimeout(function() {
-		$("#front, #back").removeClass("toreverse");
+		// $("#front, #back").removeClass("toreverse");
+		console.log("flippy");
 	}, 500);
 });
